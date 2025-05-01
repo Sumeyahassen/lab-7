6549854399dbb89f7b26facc0f2add54d8858150
Your GitHub Username <sumeyahassensumi@gmail.com>
2025-05-01 01:03:03 -0700

    database gallery

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use('/uploads', express.static('uploads'));
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '', // Password
+  password: '6979Su@+', // Password
   database: 'gallery'
 });
 
